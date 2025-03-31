9619d954e3e2b143c5512d1d293072efe34cd7a5
Jaydin McMullan <jaydin.m@hotmail.com>
2025-03-31 10:58:25 +0200

    Cleared old project and changed the back end to .Net C#

---
+++ Test Project/my-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Hello, Tailwind CSS!
+      </h1>
+      <p className="text-lg text-gray-700">Frontend is ready!</p>
+    </div>
+  );
+}
+
+export default App;
